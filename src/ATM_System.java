84264dd652eeba33187dd89367285dd1f215fdbf
kway-zar <guillplays011@gmail.com>
2025-05-26 13:48:34 +0800

    init

--- src/ATM_System.java
+++ src/ATM_System.java
@@ -1,4 +1,16 @@
 
+/**TODO:
+ * 
+    *Balance: Jonitan*
+    *Withdraw: Jeriana*
+    *Deposit: Francis*
+    *Transfer: Guil*
+    
+ **CREATE A CLASS THAT DOES THOSE FUNCTIONS(some methods can have parameters)**
+ 
+ 
+ */
+
 
 
 class ATM_System {
@@ -7,32 +19,47 @@ class ATM_System {
         return accountBalance;
     }
 
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
 
     public String getCARD_NO() {
         return CARD_NO;
-    }
-
-    public void setCARD_NO(String CARD_NO) {
-        this.CARD_NO = CARD_NO;
     }
 
     public String getPIN_CODE() {
         return PIN_CODE;
     }
 
-    public void setPIN_CODE(String PIN_CODE) {
-        this.PIN_CODE = PIN_CODE;
+    public void setUserInfo(String enteredCardNo, String enteredPIN){
+        JSON_UserInfo userInfo = new JSON_UserInfo();
+        userInfo info = new userInfo();
+        try{
+            
+            userInfo.setEnteredCardNo(enteredCardNo);
+            userInfo.setEnteredPIN(enteredPIN);
+            info = userInfo.getUserInfo();
+            this.CARD_NO = info.CARD_NO;
+            this.PIN_CODE = info.PIN_CODE;
+            this.name = info.name;
+            this.accountBalance = info.accountBalance;
+                    
+            
+            System.out.println(info.accountBalance);
+    
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
-    
-    
     
     private String CARD_NO;
     private String PIN_CODE;
+    private String name;
     private double accountBalance;
     
-    
-    
+//    public static void main(String[] args){
+//        //demonstration to check the account
+//        ATM_System atm = new ATM_System();
+//        atm.setUserInfo("5", "30");
+//   }
+//    
 }
+
--- src/JSON_UserInfo.java
+++ src/JSON_UserInfo.java
@@ -19,7 +19,16 @@ class JSON_UserInfo {
     public void setEnteredPIN(String enteredPIN) {
         this.enteredPIN = enteredPIN;
     }
-    
+    public userInfo getUserInfo(){
+        userInfo info = new userInfo();
+        try{
+            info = returnInfo();
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return info;
+    }
     private userInfo returnInfo() throws Exception{
         userInfo info = new userInfo();
         
@@ -29,97 +38,61 @@ class JSON_UserInfo {
         JSONArray ja = (JSONArray) jo.get("users");
 
         info = binarySearch(ja, "CARD_NO", enteredCardNo, enteredPIN);
-//        for(int i = 0; i < ja.size(); i++){
-//            
-//            
-//            JSONObject details = (JSONObject) ja.get(i);
-//            
-//            String cardNo = (String) details.get("CARD_NO");
-//            String pinCode = (String) details.get("PIN_CODE");
-//
-//            if (enteredCardNo.equals(cardNo) && enteredPIN.equals(pinCode)) {
-//                info.CARD_NO = cardNo;
-//                info.PIN_CODE = pinCode;
-//                info.accountBalance = (double) details.get("balance");
-//                info.name = (String) details.get("name");
-//                break;
-//            }
-//        }
-//
-//        return info;
-//       
-//    }      
+ 
+   
 
         return info;
     }
     
-    
-//    public static void main(String[] args){
-//    
-//        JSON_UserInfo json = new JSON_UserInfo();
-//        
-//        try{
-//            
-//            json.setEnteredCardNo("2");
-//            json.setEnteredPIN("3");
-//            userInfo parsedInfo = json.returnInfo();
-//            
-//            System.out.println(parsedInfo.accountBalance);
-//            
-//            
-//            
-//        } catch(Exception e){
-//            e.printStackTrace();
-//        }
-//    }
 
     public userInfo binarySearch(JSONArray arr, String target, String key, String enteredPIN){
         userInfo info = new userInfo();
         int low = 0;
         int high = arr.size() - 1;
         
-        while(low <= high){
-            int mid = low + (high - low)/ 2;
+        try {
+            while(low <= high){
+                int mid = low + (high - low)/ 2;
 
-            JSONObject element = (JSONObject)arr.get(mid);
-            String value = (String) element.get(target);
+                JSONObject element = (JSONObject)arr.get(mid);
+                String value = (String) element.get(target);
 
-            int compareResult = value.compareTo(key);
-            System.out.println(compareResult);
+                int compareResult = value.compareTo(key);
 
-            if(compareResult == 0){
-                
-               
-                String pin = (String)element.get("PIN_CODE");
-                boolean pinMatch = pin.equals(enteredPIN);
-            
-                System.out.println("PIN MATCHED:" + pinMatch);
-                System.out.println("PIN Value:" + pin);
-                if(pin.equals(enteredPIN)){
-                    info.CARD_NO = (String) element.get(target);
-                    info.PIN_CODE =(String) element.get("PIN_CODE");
-                    info.accountBalance = (double) element.get("balance");
-                    info.name = (String) element.get("name");
-                    return info;
+                if(compareResult == 0){
+
+
+                    String pin = (String)element.get("PIN_CODE");
+                    boolean pinMatch = pin.equals(enteredPIN);
+
+                    if(pin.equals(enteredPIN)){
+                        info.CARD_NO = (String) element.get(target);
+                        info.PIN_CODE =(String) element.get("PIN_CODE");
+                        info.accountBalance = (double) element.get("balance");
+                        info.name = (String) element.get("name");
+                        return info;
+                    } else {
+                        throw new customException("PIN doesn't match");
+                    }
+                    
+
+
+                } else if(compareResult > 0){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
-                
-                
-            } else if(compareResult > 0){
-                low = mid + 1;
-            } else {
-                low = mid - 1;
+
             }
-        
+            throw new customException("Invalid Credentials");
+        }catch (customException e) {
+            System.err.println("Error: " + e.getMessage());
         }
-        
         return info;
-    
     }
-    
-    
+
+
 }
-
-
 class userInfo{
     String name, CARD_NO, PIN_CODE;
     double accountBalance;
