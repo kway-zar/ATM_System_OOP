d205eab800a312e1212ac8a5515822145446ed17
Jeriana Yllen P. Maranguis <maranguisjp@students.national-u.edu.ph>
2025-05-29 21:23:46 +0800

    Button

---
+++ src/components/Button.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package components;
+
+import java.awt.*;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author rowaine
+ */
+public class Button extends JButton {
+
+    /**
+     * @return the over
+     */
+    public boolean isOver() {
+        return over;
+    }
+
+    /**
+     * @param over the over to set
+     */
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+        setBackground(color);
+    }
+
+    /**
+     * @return the colorOver
+     */
+    public Color getColorOver() {
+        return colorOver;
+    }
+
+    /**
+     * @param colorOver the colorOver to set
+     */
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+    /**
+     * @return the colorClick
+     */
+    public Color getColorClick() {
+        return colorClick;
+    }
+
+    /**
+     * @param colorClick the colorClick to set
+     */
+    public void setColorClick(Color colorClick) {
+        this.colorClick = colorClick;
+    }
+
+    /**
+     * @return the borderColor
+     */
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    /**
+     * @param borderColor the borderColor to set
+     */
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    public Button() {
+        color=Color.GRAY;
+        colorOver=new Color(255, 255, 255);
+        borderColor= new Color(255, 255, 255);
+        setContentAreaFilled(false);
+    }
+    
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color colorClick;
+    private Color borderColor;
+    private int radius = 0;
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2=(Graphics2D)grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
+ 
+        super.paintComponent(grphcs); 
+    }
+ 
+    
+}
